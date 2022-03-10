7953e8d14217d443052ecd9217891810585887dc
WachiraNjoro <dennisnwachira@gmail.com>
2022-03-10 13:49:45 -0800

    Created file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
+
 /*
- *
- * main - prints Programming is like building a multilingual puzzle
- *
- * return - returns 0 is all is success
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return - 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
@@ -11,4 +10,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
