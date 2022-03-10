0a37883cb7d4406582c39f0ea7a6b2457dd6b45c
WachiraNjoro <dennisnwachira@gmail.com>
2022-03-10 13:30:59 -0800

    Created file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *
+ * main - prints Programming is like building a multilingual puzzle
+ *
+ * return - returns 0 is all is success
+ */
+
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
