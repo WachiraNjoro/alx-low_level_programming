78b7b91ee3e74c4897504f6a469de0aecf3a89ed
WachiraNjoro <dennisnwachira@gmail.com>
2022-03-10 13:36:18 -0800

    Created file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * return - returns 0 is all is success
  */
 
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
