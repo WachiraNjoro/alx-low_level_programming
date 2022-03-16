2f85b6988bd81ba78c54f6be391755b85283cc37
WachiraNjoro <dennisnwachira@gmail.com>
2022-03-16 14:43:13 -0700

    Created file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase followed
+ * by a new line
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar ('\n');
+}
