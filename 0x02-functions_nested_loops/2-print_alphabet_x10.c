c0f32a6486d7d02a33d1e80e660119759255102c
WachiraNjoro <dennisnwachira@gmail.com>
2022-03-16 15:10:39 -0700

    Created file 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,20 @@
 
 void print_alphabet_x10(void)
 {
-	int a, b;
+	char ch;
+	int i;
 
-	for (a = 0; a < 10; a++)
+	i = 0;
+
+	while (i < 10)
 	{
-		for (b = 97; b <= 122; b++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-			_putchar(b);
-			 _putchar('\n');
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
+	}
 }
