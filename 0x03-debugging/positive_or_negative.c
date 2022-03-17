9e344faad242205a67858ace08591a8b451f209b
WachiraNjoro <dennisnwachira@gmail.com>
2022-03-17 13:28:24 -0700

    Created initial files

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+}
