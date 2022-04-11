2964e881eddde618610db102f8be01de9b9959df
WachiraNjoro <dennisnwachira@gmail.com>
2022-04-11 13:41:53 -0700

    Updated 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: 0 - success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
