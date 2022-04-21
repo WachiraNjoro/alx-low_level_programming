7e72b5cef99deb0dbc0a434f92e4e57461924cc7
WachiraNjoro <dennisnwachira@gmail.com>
2022-04-21 13:38:00 -0700

    Created file dog_h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct that keeps info about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+endif //DOG_H
